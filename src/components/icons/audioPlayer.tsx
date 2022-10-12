3720abe028b12fec7b9116575033a729636c6427
Kien Doan <doanduckien.2001@gmail.com>
2022-10-12 10:59:30 +0700

    repeat 1 song

--- src/components/icons/audioPlayer.tsx
+++ src/components/icons/audioPlayer.tsx
@@ -482,6 +482,22 @@ const ListMusic = ({ size }) => {
   );
 };
 
+const Repeat1 = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-repeat-1"
+      viewBox="0 0 16 16"
+    >
+      <path d="M11 4v1.466a.25.25 0 0 0 .41.192l2.36-1.966a.25.25 0 0 0 0-.384l-2.36-1.966a.25.25 0 0 0-.41.192V3H5a5 5 0 0 0-4.48 7.223.5.5 0 0 0 .896-.446A4 4 0 0 1 5 4h6Zm4.48 1.777a.5.5 0 0 0-.896.446A4 4 0 0 1 11 12H5.001v-1.466a.25.25 0 0 0-.41-.192l-2.36 1.966a.25.25 0 0 0 0 .384l2.36 1.966a.25.25 0 0 0 .41-.192V13h6a5 5 0 0 0 4.48-7.223Z" />
+      <path d="M9 5.5a.5.5 0 0 0-.854-.354l-1.75 1.75a.5.5 0 1 0 .708.708L8 6.707V10.5a.5.5 0 0 0 1 0v-5Z" />
+    </svg>
+  );
+};
+
 const Icon = ({ name, size = 24 }) => {
   const icons = {
     home: HomeIcon,
@@ -516,6 +532,7 @@ const Icon = ({ name, size = 24 }) => {
     window: WindowIcon,
     list: ListMusic,
     liner: Liner,
+    repeat1: Repeat1,
   };
 
   const Component = icons[name];
--- src/components/ui/navigation/audioPlayer/audioPlayer.tsx
+++ src/components/ui/navigation/audioPlayer/audioPlayer.tsx
@@ -18,8 +18,7 @@ import { data3 } from '../../../../typings/data/data';
 const AudioPlayer = () => {
   const dispatch = useDispatch();
   const [heart, setHeart] = useState(false);
-
-  const [repeat, setRepeat] = useState(false);
+  const [repeat1, setRepeat1] = useState({ index: 0 });
   const [shuffle, setShuffle] = useState(false);
   const [random, setRandom] = useState(0);
   const { current } = useSelector((state: RootState) => state.player);
@@ -27,6 +26,11 @@ const AudioPlayer = () => {
   const [audio, state, controls] = useAudio({
     src: current?.src,
   });
+
+  const handleClick = () => {
+    let i = repeat1.index < 2 ? (repeat1.index += 1) : 0;
+    setRepeat1({ index: i });
+  };
 
   useEffect(() => {
     dispatch(setSongIndex(0));
@@ -45,7 +49,7 @@ const AudioPlayer = () => {
   }, []);
 
   const updateCurrentPre = () => {
-    if (current.index === 0 && repeat) {
+    if (current.index === 0 && repeat1.index === 1) {
       dispatch(
         setCurrent(
           data3.find(value => {
@@ -78,7 +82,7 @@ const AudioPlayer = () => {
         )
       );
     } else {
-      if (current.index === data3.length - 1 && repeat) {
+      if (current.index === data3.length - 1 && repeat1.index === 1) {
         dispatch(
           setCurrent(
             data3.find(value => {
@@ -88,6 +92,8 @@ const AudioPlayer = () => {
         );
       } else if (current.index === data3.length - 1) {
         return;
+      } else if (repeat1.index === 2) {
+        dispatch(setCurrent(current));
       } else {
         dispatch(
           setCurrent(
@@ -97,13 +103,6 @@ const AudioPlayer = () => {
           )
         );
       }
-    }
-  };
-  const handleRepeat = () => {
-    if (!repeat) {
-      setRepeat(true);
-    } else {
-      setRepeat(false);
     }
   };
   const handleShuffle = () => {
@@ -132,6 +131,15 @@ const AudioPlayer = () => {
     }
   }, [state.volume, state.muted]);
 
+  const repeatIcon = useMemo(() => {
+    if (repeat1.index === 0) {
+      return 'repeat';
+    } else if (repeat1.index === 1) {
+      return 'repeat';
+    } else {
+      return 'repeat1';
+    }
+  }, [repeat1.index]);
   return (
     <div className="flex px-4 justify-between items-center h-full">
       <div className="min-w-[11.25] w-[30%]">
@@ -194,12 +202,16 @@ const AudioPlayer = () => {
             <Icon size={16} name="playerNext" />
           </button>
           <button
-            onClick={handleRepeat}
+            onClick={() => {
+              handleClick();
+            }}
             className={`w-9 h-9 flex items-center justify-center text-white ${
-              repeat ? 'text-pink-400' : 'text-opacity-70'
+              repeat1.index === 1 || repeat1.index === 2
+                ? 'text-pink-400'
+                : 'text-opacity-70'
             }  hover:text-pink-400`}
           >
-            <Icon size={16} name="repeat" />
+            <Icon size={16} name={repeatIcon} />
           </button>
         </div>
         <div className="w-full flex items-center gap-x-2 mt-1.5">
